c3de0898ef969fac822f91f19862ce016504a2fe
Mouhamed003 <mouhamedthiaw493@gmail.com>
2025-06-17 12:31:03 -0400

    Creation de mes services et component

--- src/app/services/rick-and-morty.service.ts
+++ src/app/services/rick-and-morty.service.ts
@@ -1,9 +1,19 @@
 import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
 export class RickAndMortyService {
+   private apiUrl = 'https://rickandmortyapi.com/api/character';
 
-  constructor() { }
+  constructor(private http: HttpClient) { }
+    getAllCharacters(): Observable<any> {
+    return this.http.get(`${this.apiUrl}`);
+  }
+
+  getCharacterById(id: number): Observable<any> {
+    return this.http.get(`${this.apiUrl}/${id}`);
+  }
 }
